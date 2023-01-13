0248dc2bc5481b4da902891ca35f3dcede049cae
Liam Lenholm <lenholmliam@gmail.com>
2023-01-13 16:11:42 +0100

    Redux Basics

---
+++ src/components/Login.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { login, logout } from "../features/user";
+
+function Login() {
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          dispatch(login({ name: "Liam", age: 22, email: "liam@lenholm.se" }));
+        }}
+      >
+        Login
+      </button>
+
+      <button
+        onClick={() => {
+          dispatch(logout());
+        }}
+      >
+        Logout
+      </button>
+    </div>
+  );
+}
+
+export default Login;
